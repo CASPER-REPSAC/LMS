822d4cb9a9c57e02a928dc20be88574271a91f80
신승민 <tlstmdals05@gmail.com>
2025-02-03 16:26:11 +0900

    ADD_mypage

--- src/Topbar.js
+++ src/Topbar.js
@@ -10,8 +10,7 @@ function Topbar() {
             <Link to="/">홈</Link> {/* ✅ 홈 링크 추가 */}
         </div>
         <div id='Menu_item'>
-            <Link to="/Login">로그인</Link>
-            <Link to="/New_Account">회원가입</Link>
+            <Link to="/Profile">profile</Link>
         </div>
       </nav>
       <div id="progress_bar" class="progress_bar"></div>
